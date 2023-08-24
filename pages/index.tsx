0db30c7ce5245e28a76a2106f239b4c3df77ee6e
raibbl <raibbl1997@gmail.com>
2023-08-24 03:16:49 -0400

    add a redirect to menu from homepage and sample fooditems idea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push("/menu");
+  }, []);
   return <></>;
 }
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,14 +1,151 @@
-import useMediaQuery from "@mui/material/useMediaQuery";
+import {
+  useMediaQuery,
+  Stack,
+  styled,
+  Paper,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Grid,
+} from "@mui/material";
 import { CarouselResponsive } from "../components/CarouselResponsive";
 import { BakeryName } from "../components/BakeryName";
 import { Cairo } from "@next/font/google";
 import { Button } from "antd";
 const cairoFont = Cairo({ subsets: ["latin"] });
+
+export function FoodItems(): JSX.Element {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  return (
+    <Stack spacing={2}>
+      <Card>
+        <Grid container spacing={0} columns={16}>
+          <Grid xs={8}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://thumbs.dreamstime.com/z/cat-s-half-heads-white-background-19474224.jpg"
+              title="green iguana"
+            />
+          </Grid>
+          <Grid xs={8}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+
+        <CardActions style={{ float: "right" }}>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="https://thumbs.dreamstime.com/z/cat-s-half-heads-white-background-19474224.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="https://thumbs.dreamstime.com/z/cat-s-half-heads-white-background-19474224.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="https://thumbs.dreamstime.com/z/cat-s-half-heads-white-background-19474224.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="https://thumbs.dreamstime.com/z/cat-s-half-heads-white-background-19474224.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Stack>
+  );
+}
 export default function Page({}) {
   const isMobile = useMediaQuery("(max-width:950px)");
   return (
     <main>
-      <div style={{ alignItems: "center" }}>
+      <div
+        style={{
+          alignItems: "center",
+          paddingBottom: `${isMobile ? "30" : "60"}px`,
+        }}
+      >
         <BakeryName isMobile={isMobile} fontClassName={cairoFont.className} />
         <CarouselResponsive isMobile={isMobile} />
         <Button
@@ -23,6 +160,8 @@ export default function Page({}) {
             marginLeft: "-50px",
             left: "50%",
             width: "100px",
+            height: "50px",
+            paddingBottom: "20px",
             bottom: "170px",
           }}
           className={cairoFont.className}
@@ -34,6 +173,7 @@ export default function Page({}) {
           أطلب الأن
         </Button>
       </div>
+      <FoodItems />
     </main>
   );
 }
